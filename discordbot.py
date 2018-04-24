23f81619ba1fb3ba131a57ca8687574f2690e4e7
Redclem <38690806+Redclem@users.noreply.github.com>
2018-04-24 17:27:27 +0200

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,6 @@ import asyncio
 import random
 
 client = discord.Client()
-version = "0.3"
 
 @client.event
 async def on_ready():
@@ -14,32 +13,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("!red"):
-        if message.content.startswith("!red dé"):
-            await client.send_message(message.channel,str(random.randint(1,6)))
-        elif message.content.startswith("!red rep"):
-            mess = message.content.lstrip("!red rep")
-            if not mess.startswith("^^") and not mess.startswith("!"):
-                await client.delete_message(message)
-                await client.send_message(message.channel,mess)
-        elif message.content.startswith("!red aut"):
-            await client.send_message(message.channel,message.author)
-        elif message.content.startswith("!red version"):
-            await client.send_message(message.channel,version)
-        elif message.content.startswith("!red aléa"):
-            dat = message.content.split(" ")
-            if len(dat) != 4:
-                await client.send_message(message.channel,"Erreur")
-            else:
-                try:
-                    a,b = int(dat[2]), int(dat[3])
-                except ValueError:
-                    await client.send_message(message.channel, "Erreur")
-                except:
-                    await client.send_message(message.channel, "Erreur Inattendue")
-                else:
-                    if a > b:
-                        a,b = b,a
-                    await client.send_message(message.channel,str(random.randint(a,b)))
+    if message.content.startswith("#dé"):
+        await client.send_message(message.channel,str(random.randint(1,6)))
+    elif message.content.startswith("#ano"):
+        await client.delete_message(message)
+        await client.send_message(message.content.lstrip("#ano"))
+    elif message.content == "#quit":
+        await client.logout()
+        quit()
 
-client.run("NDM4MDQ4NTkzNzc4MzEx
+client.run("NDM4MDQ4NTkzNzc4MzExMTY4.Db_I0w.2yvvfel6n860rxPA72HHYGvYUJo")
+
+input()
+client.logout()
